a33096e1dfb69cd24bfb515cf83f4ee2e0ca5f83
Ollie <94632972+sudo-ollie@users.noreply.github.com>
2025-06-03 19:09:26 +0100

    Syncing

--- src/api/middleware/security.ts
+++ src/api/middleware/security.ts
@@ -3,5 +3,5 @@ import express from 'express';
 
 export const configureSecurityMiddleware = (app: express.Application): void => {
   //    Set security headers
-  app.use(helmet());
+  // app.use(helmet());
 };
